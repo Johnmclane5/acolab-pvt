7b699c01203d10799a504fcc1e0cef8c44c44c11
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-07-09 21:19:28 +0530

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -293,9 +293,9 @@ async def format_filename(file_, user_id, dirpath=None, isMirror=False):
     return file_, cap_mono
 
 
-async def get_ss(up_path, ss_no):
+async def get_ss(up_path, ss_no, cap_mono):
     thumbs_path, tstamps = await take_ss(up_path, total=ss_no, gen_ss=True)
-    th_html = f"<h4>{ospath.basename(up_path)}</h4><br><b>Total Screenshots:</b> {ss_no}<br><br>"
+    th_html = f"<h4>{cap_mono}</h4><br><b>Total Screenshots:</b> {ss_no}<br><br>"
     th_html += ''.join(f'<img src="https://graph.org{upload_file(ospath.join(thumbs_path, thumb))[0]}"><br><pre>Screenshot at {tstamps[thumb]}</pre>' for thumb in natsorted(await listdir(thumbs_path)))
     await aiormtree(thumbs_path)
     link_id = (await telegraph.create_page(title="ScreenShots", content=th_html))["path"]
