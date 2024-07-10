27716df806413647dc6e67f372338f7275b4903d
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-07-10 10:24:58 +0530

    Update mirror_leech.py

--- bot/modules/mirror_leech.py
+++ bot/modules/mirror_leech.py
@@ -281,7 +281,6 @@ async def _mirror_leech(client, message, isQbit=False, isLeech=False, sameDir=No
     listener = MirrorLeechListener(message, compress, extract, isQbit, isLeech, tag, select, seed, sameDir, rcf, up, join, drive_id=drive_id, index_link=index_link, leech_utils={'screenshots': sshots, 'thumb': thumb})
 
     if file_ is not None:
-        name = message.caption
         await delete_links(message)
         await TelegramDownloadHelper(listener).add_download(reply_to, f'{path}/', name, session)
     elif isinstance(link, dict):
