e200d14c015223386ae0f430a27c18aa80ea3235
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-07-13 12:03:39 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -76,6 +76,6 @@ class TelegraphHelper:
         return
 
 
-telegraph = TelegraphHelper('TG-Flix', 'https://t.me/thetgflix')
+telegraph = TelegraphHelper('TG-Flix', 'https://t.me/thetgflixadult')
 
 bot_loop.run_until_complete(telegraph.create_account())
